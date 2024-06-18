c903b6a2af7137ca0dfc7f833e23e2d09a6fb98b
xuelingbo <xuelingbo@gmail.com>
2024-06-18 15:14:54 +0900

    reorganized, add bash file and make it executable

---
+++ modify.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp /modified/NoahmpIOVarType.F90 /hrldas/noahmp/drivers/hrldas/NoahmpIOVarType.F90
+cp /modified/NoahmpIOVarInitMod.F90 /hrldas/noahmp/drivers/hrldas/NoahmpIOVarInitMod.F90
+cp /modified/module_NoahMP_hrldas_driver.F /hrldas/hrldas/IO_code/module_NoahMP_hrldas_driver.F
