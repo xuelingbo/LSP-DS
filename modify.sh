6b89932ac6c2489d7f272e1483b1b1bef8cccb14
xuelingbo <xuelingbo@gmail.com>
2024-08-05 23:55:06 +0900

    Bug fixed: diagnostic T2, Q2; outputs for urban.

--- modify.sh
+++ modify.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 # 2024.06.18  Created by XUE Lingbo (CCS, Tsukuba, Japan) 
 
-cp /modified/NoahmpIOVarType.F90 /hrldas/noahmp/drivers/hrldas/NoahmpIOVarType.F90
-cp /modified/NoahmpIOVarInitMod.F90 /hrldas/noahmp/drivers/hrldas/NoahmpIOVarInitMod.F90
-cp /modified/module_NoahMP_hrldas_driver.F /hrldas/hrldas/IO_code/module_NoahMP_hrldas_driver.F
+cp ./modified/NoahmpIOVarType.F90 ./hrldas/noahmp/drivers/hrldas/NoahmpIOVarType.F90
+cp ./modified/NoahmpIOVarInitMod.F90 ./hrldas/noahmp/drivers/hrldas/NoahmpIOVarInitMod.F90
+cp ./modified/module_NoahMP_hrldas_driver.F ./hrldas/hrldas/IO_code/module_NoahMP_hrldas_driver.F
