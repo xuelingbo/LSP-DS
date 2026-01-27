89fef3467139a514f7e65e76b5127dc7e17c6664
xuelingbo <xuelingbo@gmail.com>
2026-01-27 11:53:06 +0900

    install from script

---
+++ install-hrldas.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+bash Miniconda3-latest-Linux-x86_64.sh
+
+source ~/.bashrc
+
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
+conda create -n hrldas python=3.12 -y
+
+conda activate hrldas
+
+conda install -c conda-forge cmake make -y 
+
+conda install -c conda-forge git -y
+
+conda install -c conda-forge numpy scipy matplotlib pandas xarray rioxarray -y
+
+conda install -c conda-forge netcdf4 h5py -y
+
+conda install -c conda-forge netcdf-fortran -y
+
+conda install -c conda-forge fortran-compiler -y
+
+conda install -c conda-forge cfgrib eccodes
+
+conda install -c conda-forge cdsapi
+
+git clone --recurse-submodules https://github.com/xuelingbo/LSP-DS
