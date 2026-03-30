943207fe0c5cf43aadc12d98b3d66de9f15d36cb
xuelingbo <xuelingbo@gmail.com>
2026-03-30 17:48:45 +0900

    gfs forecasting

---
+++ GFS_forced/create_forcing.py
@@ -0,0 +1,373 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+##############################################################################
+# History:
+#   2026.03.26  Created by XUE Lingbo (CCS, Tsukuba, Japan)
+##############################################################################
+
+##############################################################################
+# Create HRLDAS forcing files (LDASIN) from downloaded GFS forecast data.
+# Variable list:
+#   https://www.nco.ncep.noaa.gov/pmb/products/gfs/gfs.t00z.pgrb2.0p25.f000.shtml
+##############################################################################
+
+import numpy as np
+import os
+import pandas as pd
+import xarray as xr
+import rioxarray
+from scipy.interpolate import RectBivariateSpline
+import sys
+import matplotlib.pyplot as plot
+
+def create_LDASIN_files(start_date, cycle_hour, fhour, raw_data_dir, output_dir, geo_em_file, upper_level_m):
+    
+    if not os.path.exists(output_dir+"/LDASIN"):
+        os.makedirs(output_dir+"/LDASIN")
+
+    time = pd.to_datetime(start_date) + pd.Timedelta(hours=fhour)
+
+    filename = f"gfs.t{cycle_hour}z.pgrb2.0p25.f{fhour:03d}.grb2"
+    # sp     :  surface pressure
+    # orog   :  surface geopotential height
+    # prate  :  precipitation rate
+    ds_sfc_instant = xr.open_dataset(os.path.join(raw_data_dir, filename), engine='cfgrib', 
+                           filter_by_keys={'stepType': 'instant', 'typeOfLevel': 'surface'})
+    # prate  :  precipitation rate (accumulated over the forecast step)
+    # sdswrf :  downward short-wave radiation flux (accumulated over the forecast step)
+    # sdlwrf :  downward long-wave radiation flux (accumulated over the forecast step)
+    ds_sfc_avg = xr.open_dataset(os.path.join(raw_data_dir, filename), engine='cfgrib', 
+                           filter_by_keys={'stepType': 'avg', 'typeOfLevel': 'surface'})
+    # t      :  temperature 
+    # q      :  specific humidity
+    # u      :  u-component of wind
+    # v      :  v-component of wind
+    # gh     :  geopotential height
+    ds_pres_instant = xr.open_dataset(os.path.join(raw_data_dir, filename), engine='cfgrib',
+                           filter_by_keys={'stepType': 'instant', 'typeOfLevel': 'isobaricInhPa'})
+
+    geo_em = xr.open_dataset(geo_em_file)
+    geo_lat, geo_lon = geo_em.XLAT_M.values[0], geo_em.XLONG_M.values[0]
+    geo_lat_flat, geo_lon_flat = geo_lat.ravel(), geo_lon.ravel()
+    def subset(ds):
+        return ds.sel(latitude=slice(geo_lat.min(), geo_lat.max()),
+                      longitude=slice(geo_lon.min(), geo_lon.max()))
+    ds_sfc_instant, ds_sfc_avg, ds_pres_instant  = subset(ds_sfc_instant), subset(ds_sfc_avg), subset(ds_pres_instant)
+    raw_lat, raw_lon = ds_sfc_instant.latitude.values, ds_sfc_instant.longitude.values
+
+    vars = {
+        't':        {'ds': ds_pres_instant, 'name': 'T2D',      'attrs': {'units': 'K'}},
+        'q':        {'ds': ds_pres_instant, 'name': 'Q2D',      'attrs': {'units': 'kg/kg'}},
+        'u':        {'ds': ds_pres_instant, 'name': 'U2D',      'attrs': {'units': 'm/s'}},
+        'v':        {'ds': ds_pres_instant, 'name': 'V2D',      'attrs': {'units': 'm/s'}},
+        'sp':       {'ds': ds_sfc_instant,  'name': 'PSFC',     'attrs': {'units': 'Pa'}},
+        'sdlwrf':   {'ds': ds_sfc_avg,      'name': 'LWDOWN',   'attrs': {'units': 'W/m^2'}},
+        'sdswrf':   {'ds': ds_sfc_avg,      'name': 'SWDOWN',   'attrs': {'units': 'W/m^2'}},
+        'prate':    {'ds': ds_sfc_avg,      'name': 'RAINRATE', 'attrs': {'units': 'kg/m^2/s'}},
+        'LAI12M':   {'ds': None,            'name':'LAI',       'attrs': {'units': 'm^2/m^2'}},
+        'GREENFRAC':{'ds': None,            'name':'VEGFRA',    'attrs': {'units': '%'}},
+    }
+
+
+    LDASIN_file = xr.Dataset()
+    
+    for var in vars:
+
+        if vars[var]['ds'] is not None:
+            data_var = vars[var]['ds'][var].values
+
+        if var not in ['t', 'LAI12M', 'GREENFRAC']:
+            interp_spline = RectBivariateSpline(raw_lat, raw_lon, data_var, kx=1, ky=1)
+            data_var_interpolated = interp_spline.ev(geo_lat_flat, geo_lon_flat).reshape(geo_lat.shape)
+            LDASIN_file[vars[var]['name']] = (('Time','south_north','west_east'), [data_var_interpolated])
+            LDASIN_file[vars[var]['name']].attrs['units'] = vars[var]['attrs']['units']
+        elif var == 't':
+            gh = ds_pres_instant['gh']
+            data_var_correct_to_msl = data_var - (-0.0065 * gh / 9.80665)  
+            interp_spline = RectBivariateSpline(raw_lat, raw_lon, data_var_correct_to_msl, kx=1, ky=1)
+            data_var_interpolated = interp_spline.ev(geo_lat_flat, geo_lon_flat).reshape(geo_lat.shape)
+            data_var_correct_to_HGT_M = data_var_interpolated + ( -0.0065 * (geo_em['HGT_M'].values.squeeze()+upper_level_m))
+            LDASIN_file[vars[var]['name']] = (('Time','south_north','west_east'), [data_var_correct_to_HGT_M])
+            LDASIN_file[vars[var]['name']].attrs['units'] = vars[var]['attrs']['units']
+        elif vars[var]['name'] in ['LAI', 'VEGFRA']:
+            if pd.to_datetime(start_date).is_leap_year:
+                raw_data_file = xr.open_dataset(os.path.join(output_dir,'LDASIN', f'{var}_leap.nc'))
+                data_var = [raw_data_file[var].sel(date='2020'+start_date[-6:]).values]
+            else:
+                raw_data_file = xr.open_dataset(os.path.join(output_dir,'LDASIN', f'{var}.nc'))
+                data_var = [raw_data_file[var].sel(date='2021'+start_date[-6:]).values]
+            LDASIN_file[vars[var]['name']] = (('Time','south_north','west_east'), data_var)
+            LDASIN_file[vars[var]['name']].attrs['units'] = vars[var]['attrs']['units']
+
+    encoding=[{var: {'_FillValue': None}} for var in LDASIN_file.variables]    
+    output_filename = f"{time.strftime('%Y%m%d%H')}.LDASIN_DOMAIN{geo_em_file[-4]}"
+    LDASIN_file.to_netcdf(os.path.join(output_dir, 'LDASIN', output_filename), encoding=encoding[0])
+    print(output_filename)
+                        
+def create_setup_file(start_date, cycle_hour, raw_data_dir, output_dir, geo_em_file, lcz=0):
+    
+    if not os.path.exists(output_dir+"/LDASIN"):
+        os.makedirs(output_dir+"/LDASIN")
+
+    variables = {
+    
+        "Times": {'units':''} ,
+
+        # from geo_em file
+        "XLAT": {'units': 'degree_north', 'geoname': 'XLAT_M'} , 
+        "XLONG": {'units': 'degree_east', 'geoname': 'XLONG_M'} , 
+        "HGT": {'units': 'm', 'geoname': 'HGT_M'} , 
+        "MAPFAC_MX": {'units': '', 'geoname': 'MAPFAC_MX'} , 
+        "MAPFAC_MY": {'units': '', 'geoname': 'MAPFAC_MY'} ,  
+        "URBLANDUSEF": {'units': '', 'geoname': 'LANDUSEF'} ,      # for 2D urban fraction
+
+        # edit from geo_em file
+        "TMN": {'units': 'K', 'geoname': 'SOILTEMP'} ,  # adjust to elevation
+        "SHDMAX": {'units': '%'} ,  # max(100*GREENFRAC) 
+        "SHDMIN": {'units': '%'} ,  # min(100*GREENFRAC) 
+        "LAI": {'units': 'm^2/m^2'} , # LAI12M after interpolated
+        "XLAND": {'units': '', 'geoname': 'LU_INDEX'} ,  # if LU_INDEX==iswater or islake,2; else 1.  
+        "ISLTYP": {'units': '', 'geoname': 'SOILCTOP'} ,
+        "IVGTYP": {'units': '', 'geoname': 'LU_INDEX'} ,
+
+        # from raw data file
+        "TSK": {'units': 'K'} ,  # skin temperature from ERA5 
+        "TSLB": {'units': 'K'} ,  # soil layer temp 
+        "SMOIS": {'units': 'm^3/m^3'},  # layer volumetric total water content [m3/m3] !!!
+        "DZS": {'units': 'm'} ,  # each soil layer depth
+        "ZS": {'units': 'm'} ,   # soil layer 
+        "SNOW": {'units': 'kg/m^2'} , #snow depth
+
+        # add
+        "SEAICE": {'units': ''} ,       # sea ice fraction (=0 for a land point)
+        "CANWAT": {'units': 'kg/m^2'} , # set CANWAT = 0
+    }
+    
+    geo_em = xr.open_dataset(geo_em_file)
+    geo_lat, geo_lon = geo_em.XLAT_M.values[0], geo_em.XLONG_M.values[0]
+    geo_lat_flat, geo_lon_flat = geo_lat.ravel(), geo_lon.ravel()
+
+    iswater = int(geo_em.attrs['ISWATER'])
+    islake = int(geo_em.attrs['ISLAKE'])
+    issoilwater = int(geo_em.attrs['ISOILWATER'])
+    water_mask = (geo_em.LU_INDEX.values[0] == iswater) | (geo_em.LU_INDEX.values[0] == islake)
+
+    if pd.Timestamp(start_date).is_leap_year:
+        LAI = xr.open_dataset(os.path.join(output_dir, 'LDASIN', 'LAI12M_leap.nc'))
+    else:
+        LAI = xr.open_dataset(os.path.join(output_dir, 'LDASIN', 'LAI12M.nc'))
+
+
+    raw_data_path = os.path.join(raw_data_dir, f'gfs.t{cycle_hour}z.pgrb2.0p25.setup.grb2')
+    ds_sfc  = xr.open_dataset(raw_data_path, engine='cfgrib',
+                               backend_kwargs={'filter_by_keys': {'typeOfLevel': 'surface'}})
+    ds_soil = xr.open_dataset(raw_data_path, engine='cfgrib',
+                               backend_kwargs={'filter_by_keys': {'typeOfLevel': 'depthBelowLandLayer'}})
+    
+    def subset(ds):
+        return ds.sel(latitude=slice(geo_lat.min(), geo_lat.max()),
+                      longitude=slice(geo_lon.min(), geo_lon.max()))
+
+    ds_sfc  = subset(ds_sfc)
+    ds_soil = subset(ds_soil)
+    raw_lat, raw_lon = ds_sfc.latitude.values, ds_sfc.longitude.values
+
+    # GFS cfgrib shortNames: t=TMP, sdwe=WEASD, st=TSOIL, soilw=SOILW
+    gfs_vars = [
+        (ds_sfc,  't',   None),     # [0] skin temperature (TMP surface)
+        (ds_soil, 'soilw', 0),      # [1] soil moisture layer 1
+        (ds_soil, 'soilw', 1),      # [2] soil moisture layer 2
+        (ds_soil, 'soilw', 2),      # [3] soil moisture layer 3
+        (ds_soil, 'soilw', 3),      # [4] soil moisture layer 4
+        (ds_soil, 'st',  0),        # [5] soil temperature layer 1
+        (ds_soil, 'st',  1),        # [6] soil temperature layer 2
+        (ds_soil, 'st',  2),        # [7] soil temperature layer 3
+        (ds_soil, 'st',  3),        # [8] soil temperature layer 4
+        (ds_sfc,  'sdwe', None),    # [9] snow depth (WEASD)
+    ]
+
+    def var_interpolate(data_var):
+        data_var = data_var.squeeze() \
+                       .rio.write_crs("epsg:4326") \
+                       .rio.write_nodata(np.nan) \
+                       .rio.interpolate_na().values
+        interp_spline = RectBivariateSpline(raw_lat, raw_lon, data_var, kx=1, ky=1)
+        data_var_interpolated = interp_spline.ev(geo_lat_flat, geo_lon_flat).reshape(geo_lat.shape)
+        data_var_interpolated_masked = np.where(water_mask, np.nan, data_var_interpolated)
+        return data_var_interpolated_masked
+    
+    soil_data = []
+    for ds, varname, level_idx in gfs_vars:
+        data_var = ds[varname].isel(depthBelowLandLayer=level_idx) if level_idx is not None else ds[varname]
+        soil_data.append(var_interpolate(data_var)) 
+
+    setup_file = xr.Dataset()
+    
+    for var in variables:
+        
+        dims = ('Time', 'south_north', 'west_east')
+        dim4 = ('Time', 'soil_layers_stag', 'south_north', 'west_east')
+        dim2 = ('Time', 'soil_layers_stag')
+        
+        if var == 'Times': 
+            data_var, dims =  [pd.to_datetime(start_date).strftime('%Y-%m-%d_%H:%M:%S')], ( 'Time' )
+            
+        ##################
+        # from geo_em file
+        ##################
+        elif var in ['XLAT', 'XLONG', 'HGT', "MAPFAC_MX", "MAPFAC_MY"]: 
+            data_var = geo_em[variables[var]['geoname']].values
+        
+        elif var == 'URBLANDUSEF':
+            if lcz==0:
+                data_var = geo_em[variables[var]['geoname']].sel(land_cat=12).values       # urban
+                # if unresonable values appear, set them to 0
+                data_var[data_var < 0] = 0              
+                data_var[data_var > 1] = 0
+            else:
+                data_var = np.ones_like(geo_em[variables[var]['geoname']].sel(land_cat=0).values)
+        
+        #######################
+        # edit from geo_em file
+        #######################
+        # adjust to elevation
+        elif var == 'TMN':
+            data_var = geo_em[variables[var]['geoname']].values[0] - 0.0065 * geo_em['HGT_M'].values[0]
+            # data_var = xr.where((geo_em.LU_INDEX==iswater)|(geo_em.LU_INDEX==islake), -1.e36, data_var).values
+            data_var = [np.where(water_mask, -1.e36, data_var)]
+
+        # gvfmax%field(:,:) = maxval(geo_em%veg,3)
+        elif var == 'SHDMAX': 
+            data_var = geo_em.GREENFRAC.max(axis=1).values*100
+        
+        # gvfmin%field(:,:) = minval(geo_em%veg,3)
+        elif var == 'SHDMIN': 
+            data_var = geo_em.GREENFRAC.min(axis=1).values*100
+            
+        elif var == 'LAI':
+            if pd.Timestamp(start_date).is_leap_year:
+                data_var = [LAI.sel(date='2020'+start_date[-6:])['LAI12M'].values]
+            else:
+                data_var = [LAI.sel(date='2021'+start_date[-6:])['LAI12M'].values]
+        
+        # if LU_INDEX==iswater or islake,2; else 1.  
+        elif var == 'XLAND':
+            LU_data = geo_em[variables[var]['geoname']]
+            # data_var = xr.where((LU_data==iswater)|(LU_data==islake), 2, 1).values
+            data_var = [np.where(water_mask, 2, 1)]
+        
+        elif var == 'ISLTYP':
+            dominant_index = geo_em['SOILCTOP'].argmax(dim='soil_cat') + 1
+            dominant_value = geo_em['SOILCTOP'].max(dim='soil_cat')
+            dominant_index_corrected = xr.where(
+                (dominant_value < 0.01) | (dominant_value > 1.0), 8, dominant_index)
+            data_var = xr.where(setup_file['XLAND']==2, issoilwater, dominant_index_corrected)
+            data_var = xr.where((setup_file['XLAND']!=2)&(data_var==14), 8, data_var).values
+
+        # if urbanlandusef exist and resonable, set IVGTYP=13(urban)
+        elif var == 'IVGTYP':
+            LU = xr.where(((geo_em.LANDUSEF).sel(land_cat=12)<=1)&((geo_em.LANDUSEF).sel(land_cat=12)>0), 13, geo_em.LU_INDEX)  
+            data_var = LU.values
+
+        ####################
+        # from raw data file
+        ####################
+        elif var == 'TSK':  
+            data_var = [soil_data[0]]
+
+        elif var == 'TSLB': 
+            data_var, dims = [ np.array(soil_data[5:9]) ], dim4
+
+        elif var == 'SMOIS': 
+            data_var, dims = [ np.array(soil_data[1:5]) ], dim4
+
+        elif var == 'ZS': 
+            data_var, dims = [ [0.05, 0.25, 0.7, 1.5] ], dim2
+
+        elif var == 'DZS': 
+            data_var, dims = [ [0.1 , 0.3 , 0.6, 1 ] ], dim2
+
+        elif var == 'SNOW': 
+            data_var =  [ soil_data[-1] ]
+        
+        ########################
+        # add SEAICE and CANWAT
+        ########################
+        elif var == 'SEAICE':
+            data_var = np.zeros(geo_em.LU_INDEX.shape)
+
+        elif var == 'CANWAT': 
+            data_var = np.zeros(geo_em.LU_INDEX.shape)
+
+        print(var)
+        print(dims, np.array(data_var).shape)
+        
+        setup_file[var] = ( dims, data_var )
+
+        setup_file[var].attrs['units'] = variables[var]['units']
+    
+    setup_file = setup_file.fillna({'SNOW': -999})
+
+    setup_file.attrs = geo_em.attrs
+
+    output_filename = f"HRLDAS_setup_{pd.to_datetime(start_date).strftime('%Y%m%d')}00_d{geo_em_file[-4]}"
+    
+    setup_file.to_netcdf(os.path.join(output_dir, 'LDASIN', output_filename))
+
+def create_lai_vegfra(geo_em_file, output_dir):
+
+    if not os.path.exists(output_dir+"/LDASIN"):
+        os.makedirs(output_dir+"/LDASIN")
+
+    for var in ('LAI12M', 'GREENFRAC'):
+
+        geo = xr.open_dataset(geo_em_file)
+        LAI_geo = geo[var].sel(Time=0)
+
+        LAI = xr.concat([LAI_geo, LAI_geo, LAI_geo, LAI_geo], dim="month")
+        month = pd.date_range('2019-01-01', periods=48, freq='MS') + pd.DateOffset(days=14)
+        LAI["month"] = ("month", month)
+
+        date = pd.date_range('2019-01-15', '2022-12-15')
+        LAI=LAI.rename({'month': 'date'})
+        LAI=LAI.interp(date=date).to_dataset()
+        
+        # vegfra calibration
+        if var=='GREENFRAC':
+            LAI[var] = xr.where(LAI[var]<=0 ,0.01, LAI[var])
+            LAI = LAI * 100  
+        
+        # (iswater || islake ) == 0
+        iswater = int(geo.attrs['ISWATER'])
+        islake = int(geo.attrs['ISLAKE'])
+        LU_geo = geo['LU_INDEX'].sel(Time=0)
+        mask = ((LU_geo==iswater)|(LU_geo==islake)).expand_dims(dim={"date": date}, axis=0)
+        LAI[var] = xr.where(mask, 0, LAI[var])
+
+        if var=='LAI12M':
+            LAI.sel(date=slice('2020-01-01','2020-12-31')).to_netcdf(os.path.join(output_dir, 'LDASIN', 'LAI12M_leap.nc'))
+            LAI.sel(date=slice('2021-01-01','2021-12-31')).to_netcdf(os.path.join(output_dir, 'LDASIN', 'LAI12M.nc'))
+        else:
+            LAI.sel(date=slice('2020-01-01','2020-12-31')).to_netcdf(os.path.join(output_dir, 'LDASIN', 'GREENFRAC_leap.nc'))
+            LAI.sel(date=slice('2021-01-01','2021-12-31')).to_netcdf(os.path.join(output_dir, 'LDASIN', 'GREENFRAC.nc'))
+
+
+if __name__ == '__main__':
+
+    start_date = '2026-03-26'
+    n_days = 5
+    cycle_hour    = '00'
+    upper_level_m = 50
+
+    raw_data_dir = '../hands-on/GFS/Tokyo/raw/'
+    output_dir = '../hands-on/GFS/Tokyo/'
+    geo_em_file = '../hands-on/GFS/Tokyo/geo/geo_em.d01.nc'
+
+    create_lai_vegfra(geo_em_file, output_dir)
+    create_setup_file(start_date, cycle_hour, raw_data_dir, output_dir, geo_em_file, lcz=1)
+
+    for fhour in range(1, n_days*24+1, 1):
+        create_LDASIN_files(start_date, cycle_hour, fhour, raw_data_dir, output_dir, geo_em_file, upper_level_m)
+        
+        
---
+++ GFS_forced/download_gfs_hrldas.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#######################################################################
+# History:
+#   2026.03.26  Created by XUE Lingbo (CCS, Tsukuba, Japan)
+#######################################################################
+
+#######################################################################
+# Download GFS 0.25-degree forecast data from NOMADS NCEP for HRLDAS forcing.
+#
+# Two requests are made per forecast hour and merged into one GRIB2 file:
+#   1. Surface level  : DSWRF, DLWRF, PRATE, PRES
+#   2. 50 m above ground: TMP, SPFH, UGRD, VGRD
+#
+# NOMADS filter service:
+#   https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl
+#   https://nomads.ncep.noaa.gov/gribfilter.php?ds=gfs_0p25
+# Variable list:
+#   https://www.nco.ncep.noaa.gov/pmb/products/gfs/gfs.t00z.pgrb2.0p25.f000.shtml
+#######################################################################
+
+import os
+import re
+import requests
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
+
+NOMADS_BASE    = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl"
+GFS_S3_BUCKET  = "noaa-gfs-bdp-pds"
+
+
+def get_available_fhours(date, cycle_hour, n_days, interval):
+    """
+    Query the NOAA GFS S3 bucket and return the leading continuous list of
+    forecast hours (step=interval) that exist for the given date/cycle.
+    Stops at the first missing hour.
+
+    File naming:
+      gfs.YYYYMMDD/HH/atmos/gfs.tHHz.pgrb2.0p25.fNNN
+    """
+    s3     = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+    prefix = f"gfs.{date}/{cycle_hour}/atmos/gfs.t{cycle_hour}z.pgrb2.0p25."
+
+    available = set()
+    paginator = s3.get_paginator("list_objects_v2")
+    for page in paginator.paginate(Bucket=GFS_S3_BUCKET, Prefix=prefix):
+        for obj in page.get("Contents", []):
+            filename = obj["Key"].split("/")[-1]
+            m = re.search(r"\.f(\d{3})$", filename)
+            if m:
+                available.add(int(m.group(1)))
+
+    fhours = []
+    for h in range(1, n_days * 24 + 1, interval):
+        if h in available:
+            fhours.append(h)
+        else:
+            print(f"[info] Continuous sequence ends at f{fhours[-1]:03d} (f{h:03d} not available)")
+            break
+    return fhours
+
+
+def build_url(date, cycle_hour, fhour, variables, level_param, subregion=None):
+    """
+    Build a NOMADS filter URL for GFS 0.25-degree data.
+
+    Parameters
+    ----------
+    date        : str   e.g. "20260326"
+    cycle_hour  : str   e.g. "00", "06", "12", "18"
+    fhour       : int   forecast hour (0 = analysis)
+    variables   : list  e.g. ["DSWRF", "DLWRF", "PRATE", "PRES"]
+    level_param : str or list  NOMADS level token(s), e.g. "lev_surface" or
+                               ["lev_0-0.1_m_below_ground", "lev_0.1-0.4_m_below_ground"]
+    subregion   : dict  optional keys: toplat, leftlon, rightlon, bottomlat
+    """
+    filename = f"gfs.t{cycle_hour}z.pgrb2.0p25.f{fhour:03d}"
+
+    params = {
+        "dir": f"/gfs.{date}/{cycle_hour}/atmos",
+        "file": filename,
+    }
+    for var in variables:
+        params[f"var_{var}"] = "on"
+    for lp in ([level_param] if isinstance(level_param, str) else level_param):
+        params[lp] = "on"
+
+    if subregion:
+        params["subregion"] = ""
+        params["toplat"]    = subregion["toplat"]
+        params["leftlon"]   = subregion["leftlon"]
+        params["rightlon"]  = subregion["rightlon"]
+        params["bottomlat"] = subregion["bottomlat"]
+
+    query = "&".join(
+        f"{k}={v}" for k, v in params.items()
+    )
+    return f"{NOMADS_BASE}?{query}"
+
+
+def download_bytes(url):
+    """Download URL and return raw bytes. Raises on HTTP error."""
+    resp = requests.get(url, timeout=300)
+    resp.raise_for_status()
+    return resp.content
+
+
+def download_gfs_setup(date, cycle_hour, output_dir, subregion=None):
+    """
+    Download GFS analysis (anl) fields for model initialization:
+      - Soil temperature layers 1-4 (TSOIL)
+      - Soil moisture layers 1-4 (SOILW)
+      - Skin temperature (TMP at surface)
+      - Snow depth (WEASD: Water Equivalent of Accumulated Snow Depth [kg/m^2] )
+
+    Parameters
+    ----------
+    date       : str   e.g. "20260326"
+    cycle_hour : str   e.g. "00"
+    output_dir : str   directory where the GRIB2 file is saved
+    subregion  : dict or None
+    """
+    os.makedirs(output_dir, exist_ok=True)
+
+    out_file = os.path.join(
+        output_dir,
+        f"gfs.t{cycle_hour}z.pgrb2.0p25.setup.grb2"
+    )
+    if os.path.exists(out_file):
+        print(f"[skip] {out_file} already exists")
+        return
+
+    soil_levels = [
+        "lev_0-0.1_m_below_ground",
+        "lev_0.1-0.4_m_below_ground",
+        "lev_0.4-1_m_below_ground",
+        "lev_1-2_m_below_ground",
+    ]
+
+    # TSOIL + SOILW at all four soil layers
+    url_soil = build_url(
+        date, cycle_hour, 1,
+        ["TSOIL", "SOILW"], soil_levels,
+        subregion=subregion
+    )
+    print(f"[download] setup TSOIL+SOILW  {url_soil}")
+    data_soil = download_bytes(url_soil)
+
+    # Skin temperature + snow depth at surface
+    url_sfc = build_url(
+        date, cycle_hour, 1,
+        ["TMP", "WEASD"], "lev_surface",
+        subregion=subregion
+    )
+    print(f"[download] setup TMP+WEASD  {url_sfc}")
+    data_sfc = download_bytes(url_sfc)
+
+    combined = data_soil + data_sfc
+    with open(out_file, "wb") as f:
+        f.write(combined)
+    print(f"[saved]    {out_file}  ({len(combined)/1024:.1f} KB)")
+
+
+def download_gfs_forcing(date, cycle_hour, fhours, output_dir,
+                         upper_level="1000_mb", subregion=None):
+    """
+    Download GFS surface + upper-level forcing fields and save each
+    forecast hour as a single combined GRIB2 file.
+
+    Parameters
+    ----------
+    date        : str        e.g. "20260326"
+    cycle_hour  : str        e.g. "00"
+    fhours      : list[int]  forecast hours to download, e.g. [0, 3, 6, ...]
+    output_dir  : str        directory where GRIB2 files are saved
+    upper_level : str        NOMADS level for upper-layer winds/T/q,
+                             e.g. "1000_mb"
+    subregion   : dict or None
+                  {"toplat": 55, "leftlon": 50, "rightlon": 200, "bottomlat": 10}
+    """
+    os.makedirs(output_dir, exist_ok=True)
+
+    surface_vars    = ["DSWRF", "DLWRF", "PRATE", "PRES", "HGT"]
+    upper_vars      = ["TMP", "SPFH", "UGRD", "VGRD", "HGT"]
+    upper_lev_param = f"lev_{upper_level}"
+
+    for fhour in fhours:
+        tag = f"f{fhour:03d}"
+        out_file = os.path.join(
+            output_dir,
+            f"gfs.t{cycle_hour}z.pgrb2.0p25.{tag}.grb2"
+        )
+
+        if os.path.exists(out_file):
+            print(f"[skip] {out_file} already exists")
+            continue
+
+        # --- surface fields ---
+        url_sfc = build_url(
+            date, cycle_hour, fhour,
+            surface_vars, "lev_surface",
+            subregion=subregion
+        )
+        print(f"[download] surface  fhour={fhour:03d}  {url_sfc}")
+        data_sfc = download_bytes(url_sfc)
+
+        # --- upper-level fields (default: 50 m above ground, configurable via upper_level) ---
+        url_upper = build_url(
+            date, cycle_hour, fhour,
+            upper_vars, upper_lev_param,
+            subregion=subregion
+        )
+        print(f"[download] {upper_level}  fhour={fhour:03d}  {url_upper}")
+        data_upper = download_bytes(url_upper)
+
+        # GRIB2 messages are self-contained; concatenation produces a valid file
+        combined = data_sfc + data_upper
+        with open(out_file, "wb") as f:
+            f.write(combined)
+        print(f"[saved]    {out_file}  ({len(combined)/1024:.1f} KB)")
+
+
+if __name__ == "__main__":
+
+    # ------------------------------------------------------------------ #
+    # User settings
+    # ------------------------------------------------------------------ #
+    date       = "20260326"        # YYYYMMDD
+    cycle_hour = "00"              # GFS cycle: "00", "06", "12", "18"
+    n_days     = 20                         # number of forecast days
+    interval   = 1                         # forecast hour interval (1 or 3)
+
+    print(f"[check] Querying available forecast hours from S3 ...")
+    fhours = get_available_fhours(date, cycle_hour, n_days, interval)
+
+    # Spatial subset (set to None to download global)
+    subregion  = {
+        "toplat"   : 55,
+        "leftlon"  : 30,
+        "rightlon" : 155,
+        "bottomlat": -50,
+    }
+
+    upper_level = "1000_mb"  # e.g. "1000_mb"
+    output_dir  = f'../hands-on/GFS/Tokyo/raw/'
+    # ------------------------------------------------------------------ #
+
+    download_gfs_setup(date, cycle_hour, output_dir, subregion=subregion)
+    download_gfs_forcing(date, cycle_hour, fhours, output_dir,
+                         upper_level=upper_level, subregion=subregion)
